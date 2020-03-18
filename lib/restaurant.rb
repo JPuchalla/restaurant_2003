867e84536a3a924962d741d9185deb0984061c18
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-03-18 13:36:33 -0600

    iteration 2 complete

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,4 +5,19 @@ class Restaurant
     @name = name
     @dishes = []
   end
+
+  def closing_time(time)
+    close = opening_time.to_i + time
+    @closing_time = close.to_s + ':00'
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
+
+  def open_for_lunch?
+    if @opening_time <= '12:00'
+      p @dishes.upcase
+    end
+  end
 end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -4,49 +4,61 @@ require './lib/restaurant'
 
 class RestaurantTest < Minitest::Test
 def setup
-  @restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+  @restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+  @restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 end
   def test_it_exists
 
-    assert_instance_of Restaurant, @restaurant
+    assert_instance_of Restaurant, @restaurant1
   end
 
   def test_it_has_opening_time
 
 
-    assert_equal '10:00', @restaurant.opening_time
+    assert_equal '10:00', @restaurant1.opening_time
   end
 
   def test_it_has_name
 
 
-    assert_equal 'Fuel Cafe', @restaurant.name
+    assert_equal 'Fuel Cafe', @restaurant1.name
   end
 
   def test_it_has_dishes
-    
 
-    assert_equal [], @restaurant.dishes
+
+    assert_equal [], @restaurant1.dishes
   end
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    skip
-    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
 
-    assert_equal '18:00', restaurant1.closing_time(8)
-    assert_equal '23:00', restaurant2.closing_time(7)
+
+    assert_equal '18:00', @restaurant1.closing_time(8)
+    assert_equal '23:00', @restaurant2.closing_time(7)
   end
 
   def test_it_can_add_dishes
-    skip
-    restaurant = Restaurant.new('16:00', 'Il Poggio')
 
-    restaurant.add_dish('Burrata')
-    restaurant.add_dish('Pizzetta')
-    restaurant.add_dish('Ravioli')
 
-    assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
+    @restaurant1.add_dish('Burrata')
+    @restaurant1.add_dish('Pizzetta')
+    @restaurant1.add_dish('Ravioli')
+
+    assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], @restaurant1.dishes
   end
+
+  def test_restraunt_2_can_add_dishes
+
+    @restaurant2.add_dish('Burrata')
+    @restaurant2.add_dish('Pizzetta')
+    @restaurant2.add_dish('Ravioli')
+    assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], @restaurant2.dishes
+  end
+
+  def test_if_restraunt_is_open_for_lunch
+
+  end
+
+
 end
